051d009703ba7fb06ef1505f61f552f5ab8a24ed
lawantha <63142131+lawantha@users.noreply.github.com>
2020-04-04 14:07:58 +0530

    adde new fils

---
+++ footer.php
@@ -0,0 +1,32 @@
+<?php
+<div class="row"> 
+			<div class="col-12 col-sm-6 col-md-3 col-xl-2"> <!-----book-1------->
+				<a data-toggle="modal" href="#but"><img src="img/horse.jpg" width="148" height="180" border="1" class="mx-auto d-block" alt=""></a>
+				<p class="lead text-center">Book 1 Name</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-3 col-xl-2"> <!-----book-2------->
+				<a data-toggle="modal" href="#but"><img src="img/horse.jpg" width="148" height="180" border="1" class="mx-auto d-block" alt=""></a>
+				<p class="lead text-center">Book 2 Name</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-3 col-xl-2"> <!-----book-3------->
+				<a data-toggle="modal" href="#but"><img src="img/horse.jpg" width="148" height="180" border="1" class="mx-auto d-block" alt=""></a>
+				<p class="lead text-center">Book 3 Name</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-3 col-xl-2"> <!-----book-4------->
+				<a data-toggle="modal" href="#but"><img src="img/horse.jpg" width="148" height="180" border="1" class="mx-auto d-block" alt=""></a>
+				<p class="lead text-center">Book 4 Name</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-3 col-xl-2"> <!-----book-5------->
+				<a data-toggle="modal" href="#but"><img src="img/horse.jpg" width="148" height="180" border="1" class="mx-auto d-block" alt=""></a>
+				<p class="lead text-center">Book 5 Name</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-3 col-xl-2"> <!-----book-6------->
+				<a data-toggle="modal" href="#but"><img src="img/horse.jpg" width="148" height="180" border="1" class="mx-auto d-block" alt=""></a>
+				<p class="lead text-center">Book 6 Name</p>
+			</div>
+			<div class="col-12 col-sm-6 col-md-3 col-xl-2"> <!-----book-7------->
+				<a data-toggle="modal" href="#but"><img src="img/horse.jpg" width="148" height="180" border="1" class="mx-auto d-block" alt=""></a>
+				<p class="lead text-center">Book 7 Name</p>
+			</div>
+		</div>
+?>
